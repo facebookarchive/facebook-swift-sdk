1f435f9a3221eed055844b9393995fa91d1992b1
Florian Morello <arsonik@me.com>
2016-10-05 09:36:34 +0200

    Former FBSDKAppEvents.setPushNotificationsDeviceToken
    Added as AppEventsLogger.setPushNotifications(deviceToken: Data!)

--- Sources/Core/AppEvents/AppEventsLogger.swift
+++ Sources/Core/AppEvents/AppEventsLogger.swift
@@ -141,6 +141,18 @@ public class AppEventsLogger {
   }
 
   //--------------------------------------
+  // MARK: - Push Notification
+  //--------------------------------------
+
+  /**
+   Sets a device token to register the current application installation for push notifications.
+   - parameter deviceToken: Device token data.
+  */
+  public static func setPushNotifications(deviceToken: Data!) {
+    FBSDKAppEvents.setPushNotificationsDeviceToken(deviceToken)
+  }
+
+  //--------------------------------------
   // MARK: - Flush
   //--------------------------------------
 
