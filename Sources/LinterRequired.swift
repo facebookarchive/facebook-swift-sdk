a9ba2d086038ec647e38f62d387d38df418345a9
Joe Susnick <joesusnick@gmail.com>
2019-02-28 12:35:39 -0800

    removes objc files and files that depend on objc (#338)
    * removes objc files and files that depend on objc
    
    * shared configurations no longer rely on carthage
    
    * removing carthage
    
    * removes Vendor directory and Rakefile

---
+++ Sources/LinterRequired.swift
@@ -0,0 +1,11 @@
+//
+//  LinterRequired.swift
+//  FacebookCore
+//
+//  Created by Joe Susnick on 2/28/19.
+//  Copyright © 2019 Facebook Inc. All rights reserved.
+//
+
+import Foundation
+
+// Required by linter, delete as soon as possible.
