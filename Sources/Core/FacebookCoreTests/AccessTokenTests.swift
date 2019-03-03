b8748db7b25df1dae4b883efd8045786be9bce65
joesusnick <joesusnick@gmail.com>
2019-03-03 10:53:07 -0800

    Swiftlint fixes and general cleanup
    * removes file that used to be required by linter script
    * removes mistakenly copied reference file
    * adds relevant headers
    * ignores some specific linting violations in test files

--- Sources/Core/FacebookCoreTests/AccessTokenTests.swift
+++ Sources/Core/FacebookCoreTests/AccessTokenTests.swift
@@ -16,7 +16,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// swiftlint:disable multiline_arguments explicit_type_interface line_length
+// swiftlint:disable multiline_arguments line_length
 
 @testable import FacebookCore
 import XCTest
