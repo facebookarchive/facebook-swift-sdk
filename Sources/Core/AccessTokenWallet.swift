ba0242e8f7d858b746853192c6807da17e1a767d
joesus <joesusnick@gmail.com>
2019-03-03 15:53:41 -0800

    Updates to use swift style comments

--- Sources/Core/AccessTokenWallet.swift
+++ Sources/Core/AccessTokenWallet.swift
@@ -43,19 +43,19 @@ class AccessTokenWallet {
     self.graphRequestPiggybackManager = graphRequestPiggybackManager
   }
 
-  /**
-   The "global" access token that represents the currently logged in user.
-
-   The `currentAccessToken` is a convenient representation of the token of the
-   current user and is used by other SDK components (like `FBSDKLoginManager`).
-   */
+  ///
+  /// The "global" access token that represents the currently logged in user.
+  ///
+  /// The `currentAccessToken` is a convenient representation of the token of the
+  /// current user and is used by other SDK components (like `FBSDKLoginManager`).
+  ///
   var currentAccessToken: AccessToken? {
     return accessToken
   }
 
-  /**
-  Sets the stored access token. Passing a nil value will clear the `currentAccessToken` and delete web view cookies
-  */
+  ///
+  /// Sets the stored access token. Passing a nil value will clear the `currentAccessToken` and delete web view cookies
+  ///
   func setCurrent(_ token: AccessToken?) {
       if token != currentAccessToken {
         var userInfo: [AnyHashable: Any] = [:]
@@ -91,10 +91,9 @@ class AccessTokenWallet {
       }
   }
 
-  /**
-   Returns true if the currentAccessToken is not nil AND currentAccessToken is not expired
-
-   */
+  ///
+  /// Returns true if the currentAccessToken is not nil AND currentAccessToken is not expired
+  ///
   var isCurrentAccessTokenActive: Bool {
     guard let token = currentAccessToken else {
       return false
@@ -117,40 +116,40 @@ class AccessTokenWallet {
   }
 
   enum NotificationKeys {
-    /**
-     A key in the notification's userInfo that will be set
-     if and only if the user ID changed between the old and new tokens.
-
-     Token refreshes can occur automatically with the SDK
-     which do not change the user. If you're only interested in user
-     changes (such as logging out), you should check for the existence
-     of this key. The value is a `Bool`.
-
-     On a fresh start of the app where the SDK reads in the cached value
-     of an access token, this key will also exist since the access token
-     is moving from a null state (no user) to a non-null state (user).
-     */
+    ///
+    /// A key in the notification's userInfo that will be set
+    /// if and only if the user ID changed between the old and new tokens.
+    ///
+    /// Token refreshes can occur automatically with the SDK
+    /// which do not change the user. If you're only interested in user
+    /// changes (such as logging out), you should check for the existence
+    /// of this key. The value is a `Bool`.
+    ///
+    /// On a fresh start of the app where the SDK reads in the cached value
+    /// of an access token, this key will also exist since the access token
+    /// is moving from a null state (no user) to a non-null state (user).
+    ///
     static let FBSDKAccessTokenDidChangeUserIDKey = "FBSDKAccessTokenDidChangeUserIDKey"
 
-    /**
-     key in notification's userInfo object for getting the new token.
-
-     If there is no new token, the key will not be present.
-     */
+    ///
+    /// key in notification's userInfo object for getting the new token.
+    ///
+    /// If there is no new token, the key will not be present.
+    ///
     static let FBSDKAccessTokenChangeNewKey = "FBSDKAccessToken"
 
-    /**
-     key in notification's userInfo object for getting the old token.
-
-     If there was no old token, the key will not be present.
-     */
+    ///
+    /// key in notification's userInfo object for getting the old token.
+    ///
+    /// If there was no old token, the key will not be present.
+    ///
     static let FBSDKAccessTokenChangeOldKey = "FBSDKAccessTokenOld"
 
     // TODO: can probably move this into TokenExpirer when it is included in the project
-//    /**
-//     A key in the notification's userInfo that will be set
-//     if and only if the token has expired.
-//     */
+//    ///
+//    /// A key in the notification's userInfo that will be set
+//    /// if and only if the token has expired.
+//    ///
 //    static let FBSDKAccessTokenDidExpireKey = "FBSDKAccessTokenDidExpireKey"
   }
 
