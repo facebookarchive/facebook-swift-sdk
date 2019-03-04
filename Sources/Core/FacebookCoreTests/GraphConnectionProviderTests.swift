b242686cac131f97c46220a6f33d6d586291ad7f
joesusnick <joesusnick@gmail.com>
2019-03-04 09:21:52 -0800

    Enabling code coverage and adding dummy tests
    Adds dummy tests for classes that need a 'real' implementation. Currently they exist with unimplemented methods but this at least adds a smoke test level of coverage.

---
+++ Sources/Core/FacebookCoreTests/GraphConnectionProviderTests.swift
@@ -0,0 +1,38 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
+// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+// copy, modify, and distribute this software in source code or binary form for use
+// in connection with the web services and APIs provided by Facebook.
+//
+// As with any software that integrates with the Facebook platform, your use of
+// this software is subject to the Facebook Developer Principles and Policies
+// [http://developers.facebook.com/policy/]. This copyright notice shall be
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// swiftlint:disable line_length
+
+@testable import FacebookCore
+import XCTest
+
+class GraphConnectionProviderTests: XCTestCase {
+
+    func testProvidingMultipleConnections() {
+      let provider = GraphConnectionProvider()
+
+      guard let connection1 = provider.graphRequestConnection() as? GraphRequestConnection,
+        let connection2 = provider.graphRequestConnection() as? GraphRequestConnection else {
+          return XCTFail("A graph connection provider should be able to provider graph request connections of a known type")
+      }
+
+      XCTAssertFalse(connection1 === connection2,
+                     "A graph connection provider should return a unique connection instance per request")
+    }
+
+}
