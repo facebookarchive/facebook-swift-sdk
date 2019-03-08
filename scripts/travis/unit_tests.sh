d84272e6d71da64bd8a58f4149f6b56b1b0394c3
joesusnick <joesusnick@gmail.com>
2019-03-08 14:29:35 -0800

    Attempting to run unit test as part of travis

--- scripts/travis/unit_tests.sh
+++ scripts/travis/unit_tests.sh
@@ -22,6 +22,7 @@
 # --------------
 
 set -o pipefail
-xcodebuild build -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookCore -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
-xcodebuild build -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookLogin -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
-xcodebuild build -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookShare -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
+xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookCore -configuration Debug -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
+# TODO: Uncomment when test targets are set up for Login and Share
+# xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookLogin -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
+# xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookShare -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
