efc5e6a34e11741fd733831ce84111b653755136
Joe Susnick <joesusnick@gmail.com>
2019-04-15 14:21:34 -0700

    Networking (#403)
    * Introducing JSONParser and RemoteGraphResponseError
    
    * Adds support for fetching data from a graph connection
    
    * Adds support for converting fetched data into decodable objects while extracting encoded server errors
    
    * Adds mechanism for fetching objects using a desired type and a graph request
    
    * Using switch over if statement
    
    * Using typealias' for completions, safer optional guarding, removing duplicate reference to SampleNSError
    
    * Attempt to fix travis-ci error regarding swift version availability
    
    * Making api for fetching remote objects less verbose
    
    * Reordering arguments for greater clarity

---
+++ Sources/FacebookCoreTests/Sample Data/SampleGraphResponse.swift
@@ -0,0 +1,88 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
+// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+// copy, modify, and distribute this software in source code or binary form for use
+// in connection with the web services and APIs provided by Facebook.
+//
+// As with any software that integrates with the Facebook platform, your use of
+// this software is subject to the Facebook Developer Principles and Policies
+// [http://developers.facebook.com/policy/]. This copyright notice shall be
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// swiftlint:disable force_try force_unwrapping force_cast
+
+@testable import FacebookCore
+import Foundation
+
+enum SampleGraphResponse {
+  case empty
+  case nonJSON
+  case utf8String
+  case dictionary
+  case homogenousStringArray
+  case homogenousArrayOfDictionaries
+  case heterogeneousArray
+  case dictionaryAndError
+  case validDictionaries(count: Int)
+
+  var unserialized: Any? {
+    switch self {
+    case .empty, .nonJSON:
+      return nil
+
+    case .utf8String:
+      return "top level type"
+
+    case .dictionary:
+      return ["name": "bob"]
+
+    case .dictionaryAndError:
+      return [
+        "name": "bob"
+      ].merging(SampleRawRemoteGraphResponseError.valid) { _, _ in }
+
+    case .homogenousStringArray:
+      return ["one", "two", "three"]
+
+    case .homogenousArrayOfDictionaries:
+      return Array(
+        repeating: SampleGraphResponse.dictionary.unserialized,
+        count: 3
+      )
+
+    case .heterogeneousArray:
+      return ["one", "two", ["three": "four"]]
+
+    case .validDictionaries(let count):
+      return Array(repeating: ["name": "bob"], count: count) as Any
+    }
+  }
+
+  var data: Data {
+    switch self {
+    case .empty:
+      return Data()
+
+    case .nonJSON:
+      return withUnsafeBytes(of: 100.0) { Data($0) }
+
+    case .utf8String:
+      return (unserialized as! String).data(using: .utf8)!
+
+    case .dictionary,
+         .dictionaryAndError,
+         .homogenousStringArray,
+         .homogenousArrayOfDictionaries,
+         .heterogeneousArray,
+         .validDictionaries:
+      return try! JSONSerialization.data(withJSONObject: unserialized as Any, options: [])
+    }
+  }
+}
