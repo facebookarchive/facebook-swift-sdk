53bc4d36a00ebd8a27e0e7d07f796c5fbdb89054
joesusnick <joesusnick@gmail.com>
2019-04-17 11:11:06 -0700

    Adding ability to cache and retrieve encoded user profile

---
+++ Sources/FacebookCoreTests/Helpers/UserDefaultsSpy.swift
@@ -0,0 +1,47 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
+// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+// copy, modify, and distribute this software in source code or binary form for use
+// in connection with the web services and APIs provided by Facebook.
+//
+// As with any software that integrates with the Facebook platform, your use of
+// this software is subject to the Facebook Developer Principles and Policies
+// [http://developers.facebook.com/policy/]. This copyright notice shall be
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// swiftlint:disable force_unwrapping
+
+@testable import FacebookCore
+import Foundation
+
+class UserDefaultsSpy: UserDefaults {
+  let userDefaults: UserDefaults?
+
+  var capturedValues = [String: Any]()
+  var capturedDataRetrievalKey: String?
+
+  init(name: String) {
+    self.userDefaults = UserDefaults(suiteName: name)
+
+    super.init(suiteName: name)!
+  }
+
+  override func set(_ value: Any?, forKey defaultName: String) {
+    if let value = value {
+      capturedValues.updateValue(value, forKey: defaultName)
+    }
+    userDefaults?.set(value, forKey: defaultName)
+  }
+
+  override func data(forKey defaultName: String) -> Data? {
+    capturedDataRetrievalKey = defaultName
+    return userDefaults?.data(forKey: defaultName)
+  }
+}
