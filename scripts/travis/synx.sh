eebf21c4301ad1a4a8e630d8025c2eb0362411d3
joesusnick <joesusnick@gmail.com>
2019-04-18 08:57:50 -0700

    Adding synx analysis job of travis build

---
+++ scripts/travis/synx.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+#
+# You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+# copy, modify, and distribute this software in source code or binary form for use
+# in connection with the web services and APIs provided by Facebook.
+#
+# As with any software that integrates with the Facebook platform, your use of
+# this software is subject to the Facebook Developer Principles and Policies
+# [http://developers.facebook.com/policy/]. This copyright notice shall be
+# included in all copies or substantial portions of the software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# --------------
+# Main Script
+# --------------
+
+#shellcheck disable=SC1090
+
+set -e
+
+if [ "$TRAVIS_BUILD_STAGE_NAME" = Analysis ]; then
+  synx FacebookSwift.xcodeproj
+
+  if output=$(git status --porcelain) && [ -n "$output" ]; then
+    echo "Working directory isn't clean. Try running synx utility locally and commit the changes"
+    exit 1
+  else
+    echo "Synx has been run and directory is in order."
+  fi
+fi
