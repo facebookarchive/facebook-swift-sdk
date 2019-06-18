3ebe16b8f0497253c9d21a60a69fe25edd349cf3
joesusnick <joesusnick@gmail.com>
2019-06-18 13:30:00 -0700

    Fixes linter warning

--- Tests/Core/ServerConfigurationServiceTests.swift
+++ Tests/Core/ServerConfigurationServiceTests.swift
@@ -16,7 +16,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// swiftlint:disable line_length force_unwrapping type_body_length
+// swiftlint:disable line_length force_unwrapping
 
 @testable import FacebookCore
 import XCTest
