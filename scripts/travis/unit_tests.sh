5dbf14b1297ee54cbe8ec62ab13baf8008fea18e
joesusnick <joesusnick@gmail.com>
2019-07-27 12:16:26 -0700

    Specify os for travis build

--- scripts/travis/unit_tests.sh
+++ scripts/travis/unit_tests.sh
@@ -22,7 +22,7 @@
 # --------------
 
 set -o pipefail
-xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookCore -configuration Debug -destination "platform=iOS Simulator,name=iPhone 8 Plus" | xcpretty -c
+xcodebuild test -workspace "$XCODE_WORKSPACE" -sdk iphonesimulator -scheme FacebookCore -configuration Debug -destination "platform=iOS Simulator,OS=11.0.1,name=iPhone 8 Plus" | xcpretty -c
 # TODO: Uncomment when test targets are set up for Login and Share
 # xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookLogin -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
 # xcodebuild test -workspace $XCODE_WORKSPACE -sdk iphonesimulator -scheme FacebookShare -configuration Debug -destination "platform=iOS Simulator,name=iPhone 5" -destination "platform=iOS Simulator,name=iPhone 7 Plus" | xcpretty -c
